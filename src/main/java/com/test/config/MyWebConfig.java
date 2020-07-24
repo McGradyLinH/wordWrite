9b66a9ef03d30d9714610878ea22757c50909e0a
lx <734652785@qq.com>
2020-07-24 16:30:18 +0800

    second commit

---
+++ src/main/java/com/test/config/MyWebConfig.java
@@ -0,0 +1,17 @@
+package com.test.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Component
+public class MyWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**").excludePathPatterns("/login",
+                "/js/**","/css/**","/images/**","/fonts/**");
+    }
+}
---
+++ src/main/java/com/test/domain/Essay.java
@@ -0,0 +1,22 @@
+package com.test.domain;
+
+public class Essay {
+    private String name;
+    private Integer status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
